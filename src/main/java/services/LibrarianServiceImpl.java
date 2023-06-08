3ecb1a7e3f980e2349c7aac3284318f6b47f7e48
s21dororebe <s21dororebe@venta.lv>
2023-06-08 18:11:40 +0300

    Edit:

--- src/main/java/services/LibrarianServiceImpl.java
+++ src/main/java/services/LibrarianServiceImpl.java
@@ -4,4 +4,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class LibrarianServiceImpl implements LibrarianService {
+    @Override
+    public void insertNewBook() {
+
+    }
 }
