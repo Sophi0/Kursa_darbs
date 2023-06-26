0402ae0b82a77d3aa61538b339f904e66946d10b
Sophi0 <s21hohlsofj@venta.lv>
2023-06-26 13:10:38 +0300

    Added functions in LibrarianSerbice

--- src/main/java/services/LibrarianService.java
+++ src/main/java/services/LibrarianService.java
@@ -1,23 +1,35 @@
 package services;
 
-public interface LibrarianService {
-    void insertNewBook();
+import java.util.Collection;
 
+import lv.venta.models.Author;
+import lv.venta.models.BookGenre;
+import lv.venta.models.Exemplar;
+import lv.venta.models.ExemplarIssue;
+
+public interface LibrarianService {
+	
+    void insertNewBook() throws Exception;
+    
+    void updateBook(long id, String title, Collection<Author> author, BookGenre genre, String description, int writingYear, int quantity, Collection<Exemplar> exemplars) throws Exception;
+    
+    void deleteBookById(long idb) throws Exception;
+    
+    void deleteExemplarById(long idex) throws Exception;
+    
+    void insertNewUser(String email, String username, Collection<ExemplarIssue> exemplarIssue, float fines) throws Exception;
+    
+    void updateUser(long id, String email, String username, Collection<ExemplarIssue> exemplarIssue, float fines) throws Exception;
+    
+    void deleteUserById(long id) throws Exception; 
+    
+    
     /*insert new Book
     * creating new Book
     * adding book to book repository
     * using for loop creating exemplars of a book*/
 
-    /* insertNewBook
-    * updateBook
-    * deleteBook
-    * deleteExemplarById
-    * insertNewAuthor
-    * updateAuthor
-    * deleteAuthor
-    * insertNewUser
-    * updateUser
-    * deleteUser
+    /* 
     * **** write a message to user ---> user receives message in page allMyBooks
     * giveBook
     * returnBook
