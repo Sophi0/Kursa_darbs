ca421bf1f9009cad053e62b3503b2567aef8c81d
s21dororebe <s21dororebe@venta.lv>
2023-06-27 13:53:20 +0300

    Edit:

--- src/main/java/lv/venta/services/LibrarianServiceImpl.java
+++ src/main/java/lv/venta/services/LibrarianServiceImpl.java
@@ -1,11 +1,11 @@
 package lv.venta.services;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import org.springframework.stereotype.Service;
 import lv.venta.models.*;
 import lv.venta.repos.*;
+import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 import java.time.LocalDate;
 
@@ -26,26 +26,25 @@ public class LibrarianServiceImpl implements LibrarianService {
     @Autowired
     private IExemplarReturnRepo exemplarReturnRepo;
 
+    //FUNCTIONS - ALL...
     @Override
     public ArrayList<Book> allBooks() {
         return (ArrayList<Book>) bookRepo.findAll();
     }
-
     @Override
     public ArrayList<Exemplar> allExemplars() {
         return (ArrayList<Exemplar>) exemplarRepo.findAll();
     }
-
     @Override
     public ArrayList<Author> allAuthors() {
         return (ArrayList<Author>) authorRepo.findAll();
     }
-
     @Override
     public ArrayList<User> allUsers() {
         return (ArrayList<User>) userRepo.findAll();
     }
 
+    //INSERT FUNCTIONS
     @Override
     public void insertNewBook(String title, Author author, BookGenre genre, String description, int writingYear, int quantity) throws Exception {
         //TODO if not working -> findByTitle() and others functions are the reason
@@ -58,7 +57,21 @@ public class LibrarianServiceImpl implements LibrarianService {
             }
         } else throw new Exception("Book with this title and writingYear already exists");
     }
-
+    @Override
+    public void insertNewAuthor(String name, String surname, LocalDate dateOfBirth, LocalDate dateOfDeath) throws Exception {
+        //TODO if not working -> findByName() and others functions are the reason
+        if(!(authorRepo.findByName(name) && authorRepo.findBySurname(surname) &&
+                authorRepo.findByDateOfBirth(dateOfBirth) && authorRepo.findByDateOfDeath(dateOfDeath))){
+            authorRepo.save(new Author(name, surname, dateOfBirth, dateOfDeath));
+        } else throw new Exception("Book with this title and writingYear already exists");
+    }
+    @Override
+    public void insertNewUser(String name, String surname, String email, String username) throws Exception {
+        if(!(userRepo.findByUsername(username) && userRepo.findByEmail(email))){
+            userRepo.save(new User(name, surname, email, username));
+        } else throw new Exception("User with this username and email already exists");
+    }
+    //RETRIEVE FUNCTIONS
     @Override
     public Book retrieveBookById(long id) throws Exception {
         if(id > 0){
@@ -77,15 +90,6 @@ public class LibrarianServiceImpl implements LibrarianService {
     }
 
     @Override
-    public void insertNewAuthor(String name, String surname, LocalDate dateOfBirth, LocalDate dateOfDeath) throws Exception {
-        //TODO if not working -> findByName() and others functions are the reason
-        if(!(authorRepo.findByName(name) && authorRepo.findBySurname(surname) &&
-                authorRepo.findByDateOfBirth(dateOfBirth) && authorRepo.findByDateOfDeath(dateOfDeath))){
-            authorRepo.save(new Author(name, surname, dateOfBirth, dateOfDeath));
-        } else throw new Exception("Book with this title and writingYear already exists");
-    }
-
-    @Override
     public Author retrieveAuthorById(long id) throws Exception {
         if(id > 0){
             if(authorRepo.existsById(id)){
@@ -93,7 +97,7 @@ public class LibrarianServiceImpl implements LibrarianService {
             } else throw new Exception("There is no author with this id");
         } else throw new Exception("Invalid input id");
     }
-
+    //UPDATE
     @Override
     public void updateAuthor(long authorId, String name, String surname, LocalDate dateOfBirth, LocalDate dateOfDeath) throws Exception {
         if(authorId > 0){
@@ -106,73 +110,19 @@ public class LibrarianServiceImpl implements LibrarianService {
         } else throw new Exception("Incorrect id");
     }
     @Override
-    public void deleteAuthor(long authorId) throws Exception {
-        if(authorId > 0){
-            authorRepo.deleteByIdp(authorId);
-        } else throw new Exception("Incorrect id");
-    }
-    @Override
-    public void deleteAuthor(String name, String surname) {
-        authorRepo.deleteByNameAndSurname(name, surname);
-    }
+    public void updateBook(long id, String title, Author author, BookGenre genre, String description,
+                           int writingYear, int quantity) throws Exception {
+        if(id > 0) {
+            if(bookRepo.existsById(id)) {
+                Book temp = bookRepo.findById(id).get();
+                temp.setTitle(title);
+                temp.setAuthor(author);
+                temp.setGenre(genre);
+                temp.setDescription(description);
+                temp.setWritingYear(writingYear);
+                temp.setQuantity(quantity);
 
-    @Override
-    public void giveBook(long userId, long librarianId, long exemplarId) throws Exception {
-        if(userId > 0 && librarianId > 0 && exemplarId > 0){
-            User user = userRepo.findByIdp(userId);
-            Librarian librarian = librarianRepo.findByIdp(librarianId);
-            Exemplar exemplar = exemplarRepo.findByIdex(exemplarId);
-            exemplarIssueRepo.save(new ExemplarIssue(user, librarian, exemplar));
-            userRepo.save(user);
-            librarianRepo.save(librarian);
-            exemplarRepo.save(exemplar);
-        } else throw new Exception("Incorrect id");
-    }
-
-    @Override
-    public void returnBook(long userId, long librarianId, long exemplarId) throws Exception {
-        if(userId > 0 && librarianId > 0 && exemplarId > 0){
-            User user = userRepo.findByIdp(userId);
-            Librarian librarian = librarianRepo.findByIdp(librarianId);
-            Exemplar exemplar = exemplarRepo.findByIdex(exemplarId);
-            exemplarReturnRepo.save(new ExemplarReturn(user, librarian, exemplar));
-            userRepo.save(user);
-            librarianRepo.save(librarian);
-            exemplarRepo.save(exemplar);
-        } else throw new Exception("Incorrect id");
-    }
-
-    @Override
-    public void updateExpiringDate(long exemplarIssueId, LocalDateTime newDateTime) throws Exception {
-        if(exemplarIssueId > 0) {
-            ExemplarIssue exemplarIssue = exemplarIssueRepo.findByIdis(exemplarIssueId);
-            exemplarIssue.setExpiryDate(newDateTime);
-            exemplarIssueRepo.save(exemplarIssue);
-        } else throw new Exception("Incorrect id");
-    }
-
-    @Override
-    public void writeMessage(long userId, String message) throws Exception {
-        if(userId > 0){
-            User user = userRepo.findByIdp(userId);
-            user.setMessage(message);
-        } else throw new Exception("Incorrect id");
-    }
-
-    @Override
-	public void updateBook(long id, String title, Author author, BookGenre genre, String description,
-			int writingYear, int quantity) throws Exception {
-		if(id > 0) {
-			if(bookRepo.existsById(id)) {
-				Book temp = bookRepo.findById(id).get();
-				temp.setTitle(title);
-				temp.setAuthor(author);
-				temp.setGenre(genre);
-				temp.setDescription(description);
-				temp.setWritingYear(writingYear);
-				temp.setQuantity(quantity);
-
-				bookRepo.save(temp);
+                bookRepo.save(temp);
 
                 for(Exemplar exemplar : exemplarRepo.findByBookTitle(temp.getTitle())){
                     exemplar.getBook().setTitle(title);
@@ -185,44 +135,15 @@ public class LibrarianServiceImpl implements LibrarianService {
                     exemplarRepo.save(exemplar);
                     bookRepo.save(temp);
                 }
-			}
-			else {
-				throw new Exception("There is no book with this ID");
-			}
-		}
-		else {
-			throw new Exception("ID needs to be positive");
-		}
-		}
-
-	@Override
-	public void deleteBookById(long idb) throws Exception {
-		if(idb > 0) {
-			bookRepo.deleteById(idb);
-		}
-		else {
-			throw new Exception("ID need to be positive");
-		}
-	}
-
-	@Override
-	public void deleteExemplarById(long idex) throws Exception {
-		if(idex > 0) {
-			bookRepo.deleteById(idex);
-		}
-		else {
-			throw new Exception("ID need to be positive");
-		}
-
-	}
-
-    @Override
-    public void insertNewUser(String name, String surname, String email, String username) throws Exception {
-        if(!(userRepo.findByUsername(username) && userRepo.findByEmail(email))){
-            userRepo.save(new User(name, surname, email, username));
-        } else throw new Exception("User with this username and email already exists");
+            }
+            else {
+                throw new Exception("There is no book with this ID");
+            }
+        }
+        else {
+            throw new Exception("ID needs to be positive");
+        }
     }
-
     @Override
     public void updateUser(long id, String name, String surname, String email, String username) throws Exception {
         if(id > 0){
@@ -234,17 +155,86 @@ public class LibrarianServiceImpl implements LibrarianService {
             userRepo.save(userTemp);
         } else throw new Exception("Incorrect id");
     }
+    //DELETE FUNCTIONS
+    @Override
+    public void deleteAuthor(long authorId) throws Exception {
+        if(authorId > 0){
+            authorRepo.deleteByIdp(authorId);
+        } else throw new Exception("Incorrect id");
+    }
+    @Override
+    public void deleteAuthor(String name, String surname) {
+        authorRepo.deleteByNameAndSurname(name, surname);
+    }
+    @Override
+    public void deleteBookById(long idb) throws Exception {
+        if(idb > 0) {
+            bookRepo.deleteById(idb);
+        }
+        else {
+            throw new Exception("ID need to be positive");
+        }
+    }
+    @Override
+    public void deleteExemplarById(long idex) throws Exception {
+        if(idex > 0) {
+            bookRepo.deleteById(idex);
+        }
+        else {
+            throw new Exception("ID need to be positive");
+        }
 
+    }
+
+    //OTHER FUNCTIONS
     @Override
     public void deleteUserById(long id) throws Exception {
         if(id > 0){
             userRepo.deleteByIdp(id);
         } else throw new Exception("Incorrect id");
     }
-
     @Override
     public void deleteUserByUsername(String username) throws Exception {
         if(!(userRepo.deleteByUsername(username)))
             throw new Exception("Incorrect username");
+    }
+    @Override
+    public void giveBook(long userId, long librarianId, long exemplarId) throws Exception {
+        if(userId > 0 && librarianId > 0 && exemplarId > 0){
+            User user = userRepo.findByIdp(userId);
+            Librarian librarian = librarianRepo.findByIdp(librarianId);
+            Exemplar exemplar = exemplarRepo.findByIdex(exemplarId);
+            exemplarIssueRepo.save(new ExemplarIssue(user, librarian, exemplar));
+            userRepo.save(user);
+            librarianRepo.save(librarian);
+            exemplarRepo.save(exemplar);
+        } else throw new Exception("Incorrect id");
+    }
+    @Override
+    public void returnBook(long userId, long librarianId, long exemplarId) throws Exception {
+        if(userId > 0 && librarianId > 0 && exemplarId > 0){
+            User user = userRepo.findByIdp(userId);
+            Librarian librarian = librarianRepo.findByIdp(librarianId);
+            Exemplar exemplar = exemplarRepo.findByIdex(exemplarId);
+            exemplarReturnRepo.save(new ExemplarReturn(user, librarian, exemplar));
+            userRepo.save(user);
+            librarianRepo.save(librarian);
+            exemplarRepo.save(exemplar);
+        } else throw new Exception("Incorrect id");
+    }
+    @Override
+    public void updateExpiringDate(long exemplarIssueId, LocalDateTime newDateTime) throws Exception {
+        if(exemplarIssueId > 0) {
+            ExemplarIssue exemplarIssue = exemplarIssueRepo.findByIdis(exemplarIssueId);
+            exemplarIssue.setExpiryDate(newDateTime);
+            exemplarIssueRepo.save(exemplarIssue);
+        } else throw new Exception("Incorrect id");
+    }
+    @Override
+    public void writeMessage(long userId, String message) throws Exception {
+        if(userId > 0){
+            User user = userRepo.findByIdp(userId);
+            user.setMessage(message);
+        } else throw new Exception("Incorrect id");
     }
 }
