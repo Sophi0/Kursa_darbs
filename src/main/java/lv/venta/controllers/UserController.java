9872107c45744296cddb25438ab01a4d58c99d3a
Sophi0 <s21hohlsofj@venta.lv>
2023-06-27 23:24:43 +0300

    Corrected mistakes in two functions but the last two functions are not working

--- src/main/java/lv/venta/controllers/UserController.java
+++ src/main/java/lv/venta/controllers/UserController.java
@@ -1,5 +1,7 @@
 package lv.venta.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,6 +20,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    //WORKING
     @GetMapping(value = "/user/showAll/books/{surname}")	//localhost:8080/user/showAll/books/{surname}
 	public String getAllBooksByAuthorsSurname(Model model, @PathVariable("surname") String surname) {
 		try {
@@ -29,7 +32,8 @@ public class UserController {
 			return "error-page";
 		}
 	}
-        
+    
+    //WORKING
     @GetMapping(value = "/user/showAll/titleOfBook/{title}")	//localhost:8080/user/showAll/titleOfBook/{title}
 	public String getAllBooksByTitle(Model model, @PathVariable("title") String title) {
 		try {
@@ -42,12 +46,14 @@ public class UserController {
 		}
 	}
     
+    //WORKING
     @GetMapping(value = "/user/showAll/book")	//localhost:8080/user/showAll/book
 	public String getAllBooks(Model model) {
 		model.addAttribute("book", userService.selectAllBooks());
 		return "all-book-page";
 	}
     
+    //WORKING
     @GetMapping(value = "/user/showAll/userBooks/{id}")	//localhost:8080/user/showAll/userBooks/{id}
 	public String getAllUserBooks(Model model, @PathVariable("id") long idp) {
 		try {
@@ -60,6 +66,7 @@ public class UserController {
 		}
 	}
     
+    //NOT WORKING
     @GetMapping(value = "/user/showAll/fines/{id}")	//localhost:8080/user/showAll/fines/{id}
     public String getAllFinesByUserId(Model model, @PathVariable("id") long idp) {
     	try {
@@ -72,19 +79,21 @@ public class UserController {
 		}
     }
     
+    //NOT WORKING
     @GetMapping(value = "/user/bookAbook/{id}/{idp}")	//localhost:8080/user/bookAbook/{id}/{id}
-    public String getBookABookById(@PathVariable("idb") long idb, @PathVariable("idp") long idp, Model model) {
+    public String getBookABookById(@PathVariable("id") long idb, @PathVariable("id") long idp, Model model) {
     	model.addAttribute("book", new Book());
 		model.addAttribute("idp", idp);
 		return "book-a-book-page";
     }
     
+    //NOT WORKING
     @PostMapping("/user/bookAbook/{id}/{idp}")
-    public String postBookABookById(@Valid Book book, BindingResult result, @PathVariable("idb") long idb, @PathVariable("idp") long idp) {
+    public String postBookABookById(@PathVariable("id") long idb, @PathVariable("id") long idp, @Valid Book book, BindingResult result) {
     	if (!result.hasErrors()) {
             try {
             	userService.bookBook(idb, idp);
-                return "redirect:/user/showAll/book/" + idb + idp;
+                return "redirect:/user/showAll/book/" + idb + "/" + idp;
             } catch (Exception e) {
                 return "redirect:/error-page";
             }
