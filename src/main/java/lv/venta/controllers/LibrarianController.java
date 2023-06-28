638dd35a4dc5d0919e2179430ada1de2088636ee
s21dororebe <s21dororebe@venta.lv>
2023-06-28 18:08:07 +0300

    Edit: add-author - done

--- src/main/java/lv/venta/controllers/LibrarianController.java
+++ src/main/java/lv/venta/controllers/LibrarianController.java
@@ -231,11 +231,12 @@ public class LibrarianController {
     }
     @PostMapping("/librarian/add-author")
     public String postAddNewAuthor(@Valid Author author, BindingResult result) {
-        if(!result.hasErrors()){
+        if (!result.hasErrors()) {
             try {
                 librarianService.insertNewAuthor(author.getName(), author.getSurname(), author.getDateOfBirth(), author.getDateOfDeath());
-                return "redirect:/librarian/all-authors-page";
-            } catch (Exception e){
+                return "redirect:/librarian/all-authors";
+            } catch (Exception e) {
+                // Handle exception appropriately
                 return "redirect:/error";
             }
         } else {
