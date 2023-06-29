b08e6ed618fbb56d4264745ef3dc9de9f55e34fc
s21dororebe <s21dororebe@venta.lv>
2023-06-29 15:55:01 +0300

    Edit: added functions to find exemplar issue

--- src/main/java/lv/venta/repos/IExemplarIssueRepo.java
+++ src/main/java/lv/venta/repos/IExemplarIssueRepo.java
@@ -1,6 +1,8 @@
 package lv.venta.repos;
 
 import lv.venta.models.Exemplar;
+import lv.venta.models.Librarian;
+import lv.venta.models.User;
 import org.springframework.data.repository.CrudRepository;
 
 import lv.venta.models.ExemplarIssue;
@@ -14,4 +16,8 @@ public interface IExemplarIssueRepo extends CrudRepository<ExemplarIssue, Long>{
     ExemplarIssue findByIdis(long exemplarIssueId);
 
     ArrayList<ExemplarIssue> findAllByExemplarIdex(long idex);
+
+    ExemplarIssue findByUserAndLibrarianAndExemplar(User user, Librarian librarian, Exemplar exemplar);
+
+    ExemplarIssue findByExemplarIdex(long exemplarId);
 }
